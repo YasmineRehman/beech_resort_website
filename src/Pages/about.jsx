08f6c19087c7492327457444b0e3f8f5d991d7a2
YasmineRehman <chacharmeenu@gmail.com>
2023-12-13 22:04:34 +0500

    first commit

---
+++ src/Pages/about.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+ return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Nyange the Hank</h1>
+        <h2>1908 - Present</h2>
+      </header>
+      <main>
+        <section>
+          <h3>About Us</h3>
+          <p>Nyange the Hank is a prestigious beach club in NA with a rich history dating back to 1908. We pride ourselves on our impeccable service, exquisite culinary offerings, and stunning beachfront location.</p>
+        </section>
+        <section>
+          <h3>Showrooms</h3>
+          <p>Feel free to tour our expansive showrooms and private beach retreats to experience the breathtaking view and elegant ambiance of our esteemed establishment.</p>
+        </section>
+        <section>
+          <h3>Philosophy</h3>
+          <p>Our philosophy is simple: we aim to create an unforgettable experience for each and every guest. From our world-class cuisine to our exceptional customer service, we are dedicated to ensuring that your visit to Nyange the Hank leaves you feeling relaxed, pampered, and completely satisfied.</p>
+        </section>
+      </main>
+      <footer>
+        <p>© 2022 Nyange the Hank. All rights reserved.</p>
+      </footer>
+    </div>
+ );
+}
+
+export default App;
